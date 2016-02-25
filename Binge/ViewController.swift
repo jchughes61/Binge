4db090cdaadae559c269141ee216bc3d26e98998
jchughes61 <jch756@msstate.edu>
2016-02-24 21:12:20 -0600

    Initial Commit

---
+++ Binge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Binge
+//
+//  Created by Jacob Hughes on 2/24/16.
+//  Copyright © 2016 Jacob Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
